2e2186e4b191af189e5a25f9c91a2621b8e22716
Sergey <shornikov1407@gmail.com>
2023-03-25 19:57:56 +0300

    New commit

---
+++ client/src/main/java/client/utils/UserHandler.java
@@ -0,0 +1,12 @@
+package client.utils;
+
+import java.util.Scanner;
+
+public class UserHandler {
+
+    private Scanner userScanner;
+    private Stack<File> scriptStack = new Stack<>();
+    private Stack<Scanner> scannerStack = new Stack<>();
+
+
+}
